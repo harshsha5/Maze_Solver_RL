fa3b400bf3118548816bd98749b599a8906941d6
harshsha5 <harshbitsp@gmail.com>
2020-01-08 15:57:36 -0500

    RTDP complete

--- main.py
+++ main.py
@@ -9,33 +9,39 @@ start_state = env.s
 goal_state = env.s_goal
 
 print(env.MAP_SIZE)
+print("start_state is: ",start_state,"\t","Goal State: ",goal_state)
 V_table = np.zeros((env.MAP_SIZE, env.MAP_SIZE))
+obstacle_table = np.full((env.MAP_SIZE,env.MAP_SIZE), False, dtype=bool)
 
-# valid_states = np.zeros((1,2))
-valid_state_list = []
+# # valid_states = np.zeros((1,2))
+# valid_state_list = []
 
 ##Modifying the Value function table for obstacles and goal states
-print("Modifying V_table")
-for i in range(env.MAP_SIZE):
-  for j in range(env.MAP_SIZE):
-    pos = np.array([i,j]) 
-    if(not env._isValidPosition(pos)):
-        V_table[i][j] = -float('Inf')
-    else:
-        print(i,j)
-        # valid_states = np.vstack((valid_states,pos))
-        valid_state_list.append(pos)
+"""Uncomment this section before submitting"""
 
+# print("Modifying V_table")
+# for i in range(env.MAP_SIZE):
+#   for j in range(env.MAP_SIZE):
+#     pos = np.array([i,j]) 
+#     if(not env._isValidPosition(pos)):
+#         V_table[i][j] = -float('Inf')
+#         obstacle_table[i][j] = True
+
+# print("Modified V_table")
+# np.save("initially_modified_v_table", V_table)
+# np.save("obstacle_table", obstacle_table)
+# print("Saved v_table and obstacle _table")
+obstacle_table = np.load('obstacle_table.npy')
+V_table = np.load('initially_modified_v_table.npy')
 ipdb.set_trace()
 
 V_table[goal_state[0]][goal_state[1]] = 1000
-print("Modified V_table")
 
 total_reward = 0
 
 threshold_value = 0.5
-max_iteration_count = 1
-gamma = 0.95
+max_iteration_count = 5
+gamma = 0.9
 has_converged = True
 
 dirX = [-1,-1,-1,0,0,1,1,1]
@@ -46,19 +52,21 @@ for present_iteration_count in range(max_iteration_count):
   for i in range(env.MAP_SIZE):
     for j in range(env.MAP_SIZE):
           env.s = np.array([i,j])
-          print(env.s)
-          if(not env._isValidPosition(env.s)):
-            # print("State is invalid")
-            # ipdb.set_trace()
+          if(obstacle_table[i][j]):
             continue
           else:
+            if(i%100==0):
+                print(i,j)
+
             max_Q = -float('Inf')
             for elt in range(len(dirX)):
                 action = [dirX[elt],dirY[elt]]                       #See if we can get rid of actions which lead to out of the board configurations. Eg. the diagonal ones as you can simply move in one direction in that case. therefore using less fuel.
-                next_state, reward, done, info = env.step(action)
-                env.s = env.s = np.array([i,j])  #Since step function changes the present state. We don't want that here
+                next_state, reward, done, info = env.step(action)   #Change this. Don't take step. Instead actually figure out next state
+                env.s = np.array([i,j])  #Since step function changes the present state. We don't want that here
+                # env.s = valid_state
                 success_prob =  env._getSuccessProbability(env._getHeightDifference(env.s, next_state[:2]))
                 Q = reward + gamma*(success_prob*V_table[next_state[0]][next_state[1]] + (1-success_prob)*V_table[i][j])
+                # Q = reward + gamma*(success_prob*V_table[next_state[0]][next_state[1]] + (1-success_prob)*V_table[valid_state[0]][valid_state[1]])
                 if(Q>max_Q):
                     max_Q = Q
 
@@ -78,6 +86,7 @@ env.s = start_state
 done = False
 while not done:
     best_V_val = -float('Inf')
+    curr_state = env.s
     for elt in range(len(dirX)):
         action = [dirX[elt],dirY[elt]]                       #See if we can get rid of actions which lead to out of the board configurations. Eg. the diagonal ones as you can simply move in one direction in that case. therefore using less fuel.
         s_ = np.clip(env.s + np.array(action), 0, env.MAP_SIZE-1)
@@ -85,16 +94,17 @@ while not done:
         # env.s = curr_state
         if(V_table[s_[0]][s_[1]]>best_V_val):
             best_action = action
+            best_V_val = V_table[s_[0]][s_[1]]
 
-    print("Next state is: ",next_state,"\t","Present_state is: ",env.s,"\t Action is: ",best_action)
     if(not best_V_val>-float('Inf')):
         print("No Route to goal exists")
     else:
-       next_state, reward, done, info = env.step(action) 
+        next_state, reward, done, info = env.step(best_action) 
+        print("Next state is: ",next_state[:2],"\t","Present_state is: ",curr_state,"\t Action is: ",best_action)
 
     if(done and next_state[2:] == goal_state):
         print("Path to goal found. Goal reached successfully")
-    else:
+    elif(done):
         print("Robot fell into water")
            
 # for i in range(1000):
---
+++ rtdp.py
@@ -0,0 +1,105 @@
+import aid_gym
+import numpy as np
+import ipdb
+
+env = aid_gym.IslandEnv()
+env.reset()
+
+start_state = env.s
+goal_state = env.s_goal
+
+print(env.MAP_SIZE)
+print("start_state is: ",start_state,"\t","Goal State: ",goal_state)
+# V_table = np.zeros((env.MAP_SIZE, env.MAP_SIZE))
+# obstacle_table = np.full((env.MAP_SIZE,env.MAP_SIZE), False, dtype=bool)
+
+# # valid_states = np.zeros((1,2))
+# valid_state_list = []
+
+##Modifying the Value function table for obstacles and goal states
+"""Uncomment this section before submitting"""
+
+# print("Modifying V_table")
+# for i in range(env.MAP_SIZE):
+#   for j in range(env.MAP_SIZE):
+#     pos = np.array([i,j]) 
+#     if(not env._isValidPosition(pos)):
+#         V_table[i][j] = -float('Inf')
+#         obstacle_table[i][j] = True
+
+# print("Modified V_table")
+# np.save("initially_modified_v_table", V_table)
+# np.save("obstacle_table", obstacle_table)
+# print("Saved v_table and obstacle _table")
+# obstacle_table = np.load('obstacle_table.npy')
+V_table = np.load('initially_modified_v_table.npy')
+
+V_table[goal_state[0]][goal_state[1]] = 1000
+
+MAXIMUM_EPSIODE_LENGTH  = 2*int(np.linalg.norm(start_state-goal_state))
+print("MAXIMUM_EPSIODE_LENGTH: ",MAXIMUM_EPSIODE_LENGTH)
+MAX_EPISODES = 10
+HAS_CONVERGED = False
+GAMMA = 1
+
+dirX = [-1,-1,-1,0,0,1,1,1]
+dirY = [-1,0,1,-1,1,-1,0,1]
+
+curr_state = start_state
+done = False
+
+if(np.array_equal(start_state, goal_state)):
+    print("Goal already reached")
+    exit()
+
+num_episodes=0
+while(num_episodes<MAX_EPISODES):
+    episode_length = 0
+    num_episodes+=1
+    print("Episode: ",num_episodes)
+    while(not done and episode_length<MAXIMUM_EPSIODE_LENGTH):
+        max_Q = -float('Inf')
+        for elt in range(len(dirX)):
+            action = [dirX[elt],dirY[elt]]                    
+            next_state = np.clip(curr_state + np.array(action), 0, env.MAP_SIZE-1)
+            reward = -np.sqrt(np.square(action).sum())
+            success_prob =  env._getSuccessProbability(env._getHeightDifference(curr_state, next_state[:2]))
+            Q = reward + GAMMA*(success_prob*V_table[next_state[0]][next_state[1]] + (1-success_prob)*V_table[curr_state[0]][curr_state[1]])
+            if(Q>max_Q):
+                max_Q = Q
+                best_next_state = next_state
+
+        if(not max_Q>-float('Inf')):
+            print("No path exists")
+            break
+
+        V_table[curr_state[0]][curr_state[1]] = max_Q
+        # print(max_Q)
+        curr_state = best_next_state
+        episode_length+=1
+
+print("Training complete!")
+ipdb.set_trace()
+
+env.s = start_state
+done = False
+while not done:
+    best_V_val = -float('Inf')
+    curr_state = env.s
+    for elt in range(len(dirX)):
+        action = [dirX[elt],dirY[elt]]                       #See if we can get rid of actions which lead to out of the board configurations. Eg. the diagonal ones as you can simply move in one direction in that case. therefore using less fuel.
+        s_ = np.clip(env.s + np.array(action), 0, env.MAP_SIZE-1)
+        if(V_table[s_[0]][s_[1]]>best_V_val):
+            best_action = action
+            best_V_val = V_table[s_[0]][s_[1]]
+
+    if(not best_V_val>-float('Inf')):
+        print("No Route to goal exists")
+    else:
+        next_state, reward, done, info = env.step(best_action) 
+        print("Next state is: ",next_state[:2],"\t","Present_state is: ",curr_state,"\t Action is: ",best_action)
+
+    if(done and next_state[2:] == goal_state):
+        print("Path to goal found. Goal reached successfully")
+    elif(done):
+        print("Robot fell into water")
